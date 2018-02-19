de36fe1e8409252a71e9bc92748742a25d679a8e
Victoria Daug <victoria.daug@gmail.com>
2018-02-19 22:51:39 +0000

    To infinity

--- js2.js
+++ js2.js
@@ -4,10 +4,34 @@ Write a function called jediName which takes two arguments:
 firstName - a person's first name
 lastName - a person's last name
 The function should return the person's Jedi name. A Jedi name is formed from the first three letters of the last name, and the first two letters of the first name. For example, jediName('Beyonce', 'Knowles') should return 'KnoBe'.
-*/
 
 function jediName(firstName, lastName) {
   return lastName.substr(0,3) + firstName.substr(0, 2);
 }
 
 console.log(jediName('victoria', 'daug'))
+
+
+To infinity...
+Write a function called beyond which takes a single argument, num and logs a different message depending on its value.
+
+If num is infinite (either positive or negative) it should log out 'And beyond'.
+If num is finite and positive it should log out 'To infinity'.
+If num is finite and negative it should log out 'To negative infinity'.
+If num is zero it should log out 'Staying home'.
+*/
+
+function beyond(num) {
+  if (num === Infinity) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else {
+    console.log('Staying home')
+  }
+  return
+}
+
+beyond(Infinity)
